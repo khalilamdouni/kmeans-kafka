7f39117810849d6a5a37b841a2d507bfae296632
Khalil AMDOUNI <amdounikhalil@gmail.com>
2017-02-21 22:41:19 +0100

    Spring boot support added

---
+++ KmeansLloyd/src/main/java/com/kappa/kmeans/Application.java
@@ -0,0 +1,26 @@
+package com.kappa.kmeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The Kmeans Lloyd algorithm
+ * 
+ * @author khalil
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class Application {
+	
+	@RequestMapping("/status")
+	public String status() {
+		return "UP";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
