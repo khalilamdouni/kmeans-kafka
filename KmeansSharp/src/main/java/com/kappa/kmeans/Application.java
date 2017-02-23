48704f6a8c636fb36eef05ddcaee90170a4682f7
Khalil AMDOUNI <amdounikhalil@gmail.com>
2017-02-23 23:26:23 +0100

    Spring boot configurations

--- KmeansSharp/src/main/java/com/kappa/kmeans/Application.java
+++ KmeansSharp/src/main/java/com/kappa/kmeans/Application.java
@@ -1,5 +1,6 @@
 package com.kappa.kmeans;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class Application {
 	
+
+	@Value("${app.algorithm}")
+	private String algoName;
+
 	@RequestMapping("/status")
 	public String status() {
-		return "UP";
+		return algoName + "UP";
 	}
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
